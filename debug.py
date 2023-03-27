9e0114b4661882f6841d2f0d98f15f17d392bbfb
bluelisandro <nano0117@gmail.com>
2023-03-27 16:21:02 -0400

    add minheap func

--- debug.py
+++ debug.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from typing import Dict
-from collections import defaultdict
 import heapq
 
 # 1. Create frequency tree for each byte
@@ -13,11 +12,19 @@ def frequencyTree(message: bytes) -> Dict:
     :param message: raw sequence of bytes from a file
     :returns: dict containing the frequency of each byte in the file
     """
-    freq = defaultdict(int)
-    for byte in message:
-        freq[byte] += 1
+    # freq = {}
+    # for byte in message:
+    #     freq[byte] = freq.get(byte, 0) + 1
+    # return freq
+    return {byte: message.count(byte) for byte in set(message)}
 
-    return freq
+def minHeap(freq: Dict) -> list:
+    """ Given a dictionary containing the frequency of each byte in a file, returns a minheap containing the frequency tree for each byte.
+
+    :param freq: dict containing the frequency of each byte in a file
+    :returns: minheap containing the frequency tree for each byte
+    """
+    return heapq.heapify([(v, k) for k, v in freq.items()])
 
 
 if __name__ == '__main__':
@@ -28,5 +35,8 @@ if __name__ == '__main__':
     with open(infile, 'rb') as fp:
         message = fp.read()
         freq = frequencyTree(message)
+        print("Freuency Tree:")
         for k, v in freq.items():
             print(chr(k), v)
+        minheap = minHeap(freq)
+        print("Minheap:")
