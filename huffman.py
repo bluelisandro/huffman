60f93620be2abe5c1295e458d2841abe37e1093c
bluelisandro <nano0117@gmail.com>
2023-03-28 16:36:24 -0400

    Fix decompress and compress that are supposed to deal with raw byte arrays

--- huffman.py
+++ huffman.py
@@ -10,26 +10,7 @@ import heapq
 # ------------------------- ENCODING -------------------------
 
 
-def encode(message: bytes) -> Tuple[str, Dict]:
-    """ Given the bytes read from a file, encodes the contents using the Huffman encoding algorithm.
-    :param message: raw sequence of bytes from a file
-    :returns: string of 1s and 0s representing the encoded message
-              dict containing the decoder ring as explained in lecture and handout.
-    """
-    freq = freq_tree(message)
-    freq_min_heap = freq_min_heap(freq)
-    huffman_tree = huffman_tree(freq_min_heap)
-    encoder_ring = encoder_ring(huffman_tree)
-    
-    # encoded_bytes = "".join([codes[byte] for byte in message])
-    encoded_bytes = ""
-    for byte in message:
-        encoded_bytes += encoder_ring[byte]
-
-    return encoded_bytes
-
-
-def freq_tree(message: bytes) -> Dict:
+def create_freq_tree(message: bytes) -> Dict:
     """ Given the bytes read from a file, returns a dictionary containing the frequency of each byte in the file.
 
     :param message: raw sequence of bytes from a file
@@ -38,7 +19,7 @@ def freq_tree(message: bytes) -> Dict:
     return {byte: message.count(byte) for byte in set(message)}
 
 
-def freq_min_heap(freq: Dict) -> list:
+def create_freq_min_heap(freq: Dict) -> list:
     """ Given a dictionary containing the frequency of each byte in a file, returns a minheap containing the frequency tree for each byte.
 
     :param freq: dict containing the frequency of each byte in a file
@@ -52,7 +33,7 @@ def freq_min_heap(freq: Dict) -> list:
     return freq_min_heap
 
 
-def huffman_tree(freq_min_heap: list):
+def create_huffman_tree(freq_min_heap: list):
     """ Given a minheap containing the frequency tree for each byte, returns a huffman tree.
 
     :param minheap: minheap containing the frequency tree for each byte
@@ -74,7 +55,7 @@ def huffman_tree(freq_min_heap: list):
     return freq_min_heap[0]
 
 
-def encoder_ring(huffman_tree) -> Dict:
+def create_encoder_ring(huffman_tree) -> Dict:
     """ Given a huffman tree, returns an encoder ring.
 
     :param message: raw sequence of bytes from a file
@@ -82,6 +63,7 @@ def encoder_ring(huffman_tree) -> Dict:
               dict containing the decoder ring as explained in lecture and handout.
     """
     codes = {}
+
     def _encoder_ring(node: Tuple[int, any, any], code: str):
         # If a node has only one child, its left child is a leaf node
         # so we can add that code to the dictionary
@@ -93,6 +75,42 @@ def encoder_ring(huffman_tree) -> Dict:
     _encoder_ring(huffman_tree, '')
     return codes
 
+
+def encode(message: bytes) -> Tuple[str, Dict]:
+    """ Given the bytes read from a file, encodes the contents using the Huffman encoding algorithm.
+    :param message: raw sequence of bytes from a file
+    :returns: string of 1s and 0s representing the encoded message
+              dict containing the decoder ring as explained in lecture and handout.
+    """
+    freq = create_freq_tree(message)
+    freq_min_heap = create_freq_min_heap(freq)
+    huffman_tree = create_huffman_tree(freq_min_heap)
+    decoder_ring = create_encoder_ring(huffman_tree)
+
+    # encoded_bytes = "".join([codes[byte] for byte in message])
+    encoded_str = ""
+    for byte in message:
+        encoded_str += decoder_ring[byte]
+
+    return (encoded_str, decoder_ring)
+
+
+def compress(message: bytes) -> Tuple[array, Dict]:
+    """ Given the bytes read from a file, calls encode and turns the string into an array of bytes to be written to disk.
+
+    :param message: raw sequence of bytes from a file
+    :returns: array of bytes to be written to disk
+              dict containing the decoder ring
+    """
+    encoded_str, decoder_ring = encode(message)
+    encoded_bytes = encoded_str.encode()
+    encoded_bytes = array('B', encoded_bytes)
+    return (encoded_bytes, decoder_ring)
+
+# NOTE: Encode returns the encoded bytes as a string,
+# whereas compress returns the encoded bytes as an array of bytes.
+# So I need to figure out how to convert the string to an array of bytes.
+
 # ------------------------- DECODING -------------------------
 
 
@@ -103,30 +121,48 @@ def decode(message: str, decoder_ring: Dict) -> bytes:
     :param decoder_ring: dict containing the decoder ring
     return: raw sequence of bytes that represent a decoded file
     """
-    raise NotImplementedError
+    decoded_message = []
+    encoded_str = str(message)
+    buffer = ''
+    flipped_codes = {v: k for k, v in decoder_ring.items()}
 
-
-def compress(message: bytes) -> Tuple[array, Dict]:
-    """ Given the bytes read from a file, calls encode and turns the string into an array of bytes to be written to disk.
-
-    :param message: raw sequence of bytes from a file
-    :returns: array of bytes to be written to disk
-              dict containing the decoder ring
-    """
-    return encode(message)
+    for bit in encoded_str:
+        # print(bit)
+        buffer += bit
+        if buffer in flipped_codes:
+            decoded_message.append(flipped_codes[buffer])
+            buffer = ''
+            
+    return array('B', decoded_message)
 
 
 def decompress(message: array, decoder_ring: Dict) -> bytes:
-    """ Given a decoder ring and an array of bytes read from a compressed file, turns the array into a string and calls decode.
+    """ Given a decoder ring and an array of bytes read from a compressed file, 
+    turns the array into a string and calls decode.
 
     :param message: array of bytes read in from a compressed file
     :param decoder_ring: dict containing the decoder ring
     :return: raw sequence of bytes that represent a decompressed file
     """
-    raise NotImplementedError
+    decompressed_str = ""
+    buf = 0
+    for bit in message:
+        if bit == '0':
+            buf = buf << 1
+        else:
+            buf = (buf << 1) | 1
+    
+    return decode(buf, decoder_ring)
+
+
+# NOTE: Similar to the difference between encode and compress,
+# decode is given a the bytes as a string,
+# whereas decompress is given the decoded message as an array of bytes.
+# So I need to figure out how to convert the provided array of bytes to a string.
 
 
 # ------------------------- MAIN -------------------------
+
 if __name__ == '__main__':
     usage = f'Usage: {sys.argv[0]} [ -c | -d | -v | -w ] infile outfile'
     if len(sys.argv) != 4:
